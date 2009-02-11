971f99c7f18d3909aa65d6225cd0e9c999f90913
james.yum <james.yum@fdf3ee04-f7d5-11dd-aa46-671127399dce>
2009-02-11 00:58:24 +0000

    initial code import.

---
+++ src/com/dart/android/twitter/TwitterApi.java
@@ -0,0 +1,173 @@
+package com.dart.android.twitter;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.auth.AuthSchemeRegistry;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.auth.BasicSchemeFactory;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+public class TwitterApi {
+  private static final String TAG = "TwitterApi";
+
+  private static final String UPDATE_URL =
+    "http://twitter.com/statuses/update.json";
+  private static final String VERIFY_CREDENTIALS_URL =
+    "https://twitter.com/account/verify_credentials.json";
+  private static final String FRIENDS_TIMELINE_URL =
+    "http://twitter.com/statuses/friends_timeline.json";
+
+  private static final String TWITTER_HOST = "twitter.com";
+
+  private DefaultHttpClient mClient;
+  private AuthScope mAuthScope;
+  
+  private static final String METHOD_GET = "GET";
+  private static final String METHOD_POST = "POST";
+
+  public class AuthException extends Exception {    
+  }
+    
+  
+  public TwitterApi() {
+    prepareHttpClient();
+  }
+
+  private void prepareHttpClient() {
+    mAuthScope = new AuthScope(TWITTER_HOST, AuthScope.ANY_PORT);
+    mClient = new DefaultHttpClient();
+    BasicScheme basicScheme = new BasicScheme(); 
+    AuthSchemeRegistry authRegistry = new AuthSchemeRegistry(); 
+    authRegistry.register(basicScheme.getSchemeName(),
+        new BasicSchemeFactory()); 
+    mClient.setAuthSchemes(authRegistry); 
+    mClient.setCredentialsProvider(new BasicCredentialsProvider());     
+  }
+    
+  public void setCredentials(String username, String password) {
+    mClient.getCredentialsProvider().setCredentials( 
+        mAuthScope, 
+        new UsernamePasswordCredentials(username, password));         
+  }
+   
+  private InputStream requestData(String url, String httpMethod,
+      ArrayList<NameValuePair> params) throws
+      IOException, AuthException {
+    Log.i(TAG, "Sending " + httpMethod + " request to " + url);
+    
+    URI uri;
+    
+    try {
+      uri = new URI(url);
+    } catch (URISyntaxException e) {
+      e.printStackTrace();
+      throw new IOException("Invalid URL.");
+    }
+    
+    HttpUriRequest method;
+
+    if (METHOD_POST.equals(httpMethod)) {
+      HttpPost post = new HttpPost(uri);
+      // See this:
+      // http://groups.google.com/group/twitter-development-talk/browse_thread/thread/e178b1d3d63d8e3b
+      post.getParams().setBooleanParameter("http.protocol.expect-continue",
+          false);                 
+      post.setEntity(new UrlEncodedFormEntity(params, HTTP.UTF_8));
+      method = post;
+    } else {
+      method = new HttpPost(uri);            
+    }
+    
+    HttpResponse response;
+    
+    try {
+      response = mClient.execute(method);
+    } catch (ClientProtocolException e) {
+      e.printStackTrace();
+      throw new IOException("HTTP protocol error.");
+    }              
+    
+    int statusCode = response.getStatusLine().getStatusCode();
+      
+    if (statusCode == 401) {
+      throw new AuthException();      
+    } else if (statusCode != 200) {
+      throw new IOException("Non OK response code: " + statusCode);
+    }
+    
+    return response.getEntity().getContent();
+  }
+  
+  public void login(String username, String password) throws
+      IOException, AuthException {
+    Log.i(TAG, "Login attempt for " + username);
+    setCredentials(username, password);
+    InputStream data = requestData(VERIFY_CREDENTIALS_URL, METHOD_GET, null);    
+    data.close();
+  }
+
+  public void logout() {
+    setCredentials("", "");
+  }
+
+  public JSONArray getTimeline() throws IOException, AuthException {
+    Log.i(TAG, "Requesting friends timeline.");
+    
+    InputStream data = requestData(FRIENDS_TIMELINE_URL, METHOD_GET, null);        
+    JSONArray json = null;
+    
+    try {
+      json = new JSONArray(Utils.stringifyStream(data));
+    } catch (JSONException e) {
+      e.printStackTrace();
+      throw new IOException("Could not parse JSON.");
+    } finally {      
+      data.close();
+    }
+        
+    return json;
+  }  
+
+  public JSONObject update(String status) throws IOException, AuthException {
+    Log.i(TAG, "Updating status.");
+    
+    ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
+    params.add(new BasicNameValuePair("status", status));    
+    
+    InputStream data = requestData(UPDATE_URL, METHOD_POST, params);
+    JSONObject json = null;
+    
+    try {
+      json = new JSONObject(Utils.stringifyStream(data));
+    } catch (JSONException e) {
+      e.printStackTrace();
+      throw new IOException("Could not parse JSON.");
+    } finally {      
+      data.close();
+    }
+        
+    return json;
+  }  
+  
+}
---
+++ src/com/dart/android/twitter/TwitterService.java
@@ -0,0 +1,133 @@
+package com.dart.android.twitter;
+
+import java.io.IOException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.os.Binder;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.dart.android.twitter.TwitterApi.AuthException;
+
+public class TwitterService extends Service {
+
+  private static final String TAG = "TwitterService";
+  
+  private static final int REFRESH_INTERVAL = 10 * 1000;
+
+  public class LocalBinder extends Binder {
+    TwitterService getService() {
+      return TwitterService.this;
+    }
+  }
+  
+  private TwitterApi mApi;
+    
+  private final IBinder mBinder = new LocalBinder();
+
+  private Twitter mClient;
+    
+  private SharedPreferences mPreferences;    
+  private SharedPreferences.Editor mEditor;
+  
+  private TwitterDbAdapter mDbHelper;    
+  
+  private static final String PREFERENCES_USERNAME_KEY = "username";
+  private static final String PREFERENCES_PASSWORD_KEY = "password";
+    
+  private void setCredentials(String username, String password) {
+    mEditor.putString(PREFERENCES_USERNAME_KEY, username);
+    mEditor.putString(PREFERENCES_PASSWORD_KEY, password);
+    mEditor.commit();
+  }
+  
+  public boolean hasCredentials() {
+    String username = mPreferences.getString(PREFERENCES_USERNAME_KEY, "");
+    String password = mPreferences.getString(PREFERENCES_PASSWORD_KEY, "");
+    
+    return Twitter.isValidCredentials(username, password);
+  }
+
+  public void login(String username, String password) throws 
+      IOException, AuthException {
+    mApi.login(username, password);    
+    setCredentials(username, password);
+  }
+  
+  public void logout() {
+    setCredentials("", "");
+    mDbHelper.deleteAllTweets();        
+  }  
+  
+  @Override
+  public IBinder onBind(Intent intent) {
+    return mBinder;
+  }
+  
+  public void setClient(Twitter client) {
+    mClient = client;
+  }
+  
+  public void retrieve() throws IOException, AuthException {
+  }
+
+  
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    mDbHelper = new TwitterDbAdapter(this);
+    mDbHelper.open();
+            
+    mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+    mEditor = mPreferences.edit();    
+        
+    String username = mPreferences.getString(PREFERENCES_USERNAME_KEY, "");
+    String password = mPreferences.getString(PREFERENCES_PASSWORD_KEY, "");
+    
+    mApi = new TwitterApi();
+    
+    if (Twitter.isValidCredentials(username, password)) {
+      mApi.setCredentials(username, password);
+    }
+    
+    mHandler.sendEmptyMessage(0);    
+  }
+  
+  @Override
+  public void onDestroy() {
+    // Right order?
+    super.onDestroy();
+    
+    mDbHelper.close();
+    Log.i(TAG, "IM DYING");
+  }
+     
+  private int mValue = 0;
+  
+  private final Handler mHandler = new Handler() {
+    @Override public void handleMessage(Message msg) {
+      Log.i(TAG, ++mValue + "");
+              
+      sendMessageDelayed(obtainMessage(0), REFRESH_INTERVAL);
+    }
+  };
+
+  public void send(String text) {
+  }
+
+  public Cursor fetchAllTweets() {
+    return mDbHelper.fetchAllTweets();
+  }
+  
+}
