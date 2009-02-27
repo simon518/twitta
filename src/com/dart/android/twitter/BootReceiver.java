d91a56666723c1a3b92f97b7436edd5a509a29d6
james.yum <james.yum@fdf3ee04-f7d5-11dd-aa46-671127399dce>
2009-02-27 07:50:42 +0000

    Clen up cache

--- src/com/dart/android/twitter/BootReceiver.java
+++ src/com/dart/android/twitter/BootReceiver.java
@@ -3,13 +3,17 @@ package com.dart.android.twitter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 /**
  * This broadcast receiver is awoken after boot and registers the service that
  * checks for new tweets.
  */
 public class BootReceiver extends BroadcastReceiver {
+  private static final String TAG = "BootReceiver";
+  
   public void onReceive(Context context, Intent intent) {
+    Log.i(TAG, "Twitta BootReceiver is receiving.");
     if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {      
       TwitterService.schedule(context);
     }
