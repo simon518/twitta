1bf6a5e5b3268b024970559558fa643ea2fc6dee
james.yum <james.yum@fdf3ee04-f7d5-11dd-aa46-671127399dce>
2009-03-21 21:48:21 +0000

    taskifying delete DM action.

---
+++ trunk/src/com/dart/android/twitter/TweetEdit.java
@@ -0,0 +1,72 @@
+package com.dart.android.twitter;
+
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.Selection;
+import android.text.TextWatcher;
+import android.view.View.OnKeyListener;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class TweetEdit {
+  private EditText mEditText;
+  private TextView mCharsRemainText;
+  
+  TweetEdit(EditText editText, TextView charsRemainText) {
+    mEditText = editText;
+    mCharsRemainText = charsRemainText;
+    
+    mEditText.addTextChangedListener(mTextWatcher);
+    mEditText.setFilters(new InputFilter[] { new InputFilter.LengthFilter(
+        MAX_TWEET_INPUT_LENGTH) });        
+  }  
+
+  private static final int MAX_TWEET_LENGTH = 140;
+  private static final int MAX_TWEET_INPUT_LENGTH = 400;
+  
+  public void setTextAndFocus(String text) {
+    setText(text);
+    Editable editable = mEditText.getText();
+    Selection.setSelection(editable, editable.length());
+    mEditText.requestFocus();    
+  }
+
+  public void setText(String text) {
+    mEditText.setText(text);
+    updateCharsRemain();
+  }
+  
+  private TextWatcher mTextWatcher = new TextWatcher() {
+    @Override
+    public void afterTextChanged(Editable e) {
+      updateCharsRemain();
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count,
+        int after) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+    }
+  };
+
+  public void updateCharsRemain() {
+    int remaining = MAX_TWEET_LENGTH - mEditText.length();
+    mCharsRemainText.setText(remaining + "");
+  }
+
+  public String getText() {
+    return mEditText.getText().toString();
+  }
+
+  public void setEnabled(boolean b) {
+    mEditText.setEnabled(b);
+  }
+
+  public void setOnKeyListener(OnKeyListener listener) {
+    mEditText.setOnKeyListener(listener);    
+  }
+    
+}
